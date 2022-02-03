02219c3f0e29aa6ba165974caf7d7b1aca949b8f
Nikolaj <nikolajweinell@hotmail.com>
2022-02-03 09:30:50 +0100

    Pipeline setup, test setup

---
+++ src/test/java/UserInfoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserInfoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void seeUserNames() {
+
+
+
+
+    }
+
+
+
+}
